f38e7624d1679e7d9d5a55ab49167c2c4fac831c
TashiroRenya <22cu0125@jec.ac.jp>
2024-06-04 13:58:06 +0900

    衝突を回避

---
+++ Assets/UFE/Engine/Scripts/UI_Templates/DefaultContinueScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UFE3D;
+
+public class DefaultContinueScreen : StoryModeContinueScreen
+{
+    public AudioClip countdownSound;
+
+    #region public override methods
+    public override void OnShow()
+    {
+        base.OnShow();
+        if (this.countdownSound != null)
+        {
+            UFE.DelayLocalAction(delegate () { UFE.PlaySound(this.countdownSound); }, this.delayBeforePlayingMusic);
+        }
+    }
+    #endregion
+}
