c2a2efa093ef84084b3f76d6f2c053a4cb7c04cb
buruzonniemi <22cu0125@jec.ac.jp>
2024-06-11 00:48:44 +0900

    UFEのC#学び中

---
+++ Assets/inGame/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UFE3D;
+
+public class PlayerMove : MonoBehaviour
+{
+    //変数宣言
+    //プレイヤーのキャラクターを格納
+    [SerializeField]
+    private UFE3D.CharacterInfo player1;
+    [SerializeField]
+    private UFE3D.CharacterInfo player2;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/inGame/StartInGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UFE3D;
+
+public class StartInGame : MonoBehaviour
+{
+    //変数宣言
+    //キャラの取得
+    [SerializeField]
+    UFE3D.CharacterInfo playerCharaInfo1;
+    [SerializeField]
+    UFE3D.CharacterInfo playerCharaInfo2;
+
+    void Start()
+    {
+        // プレイヤー情報の設定
+        if (playerCharaInfo1 != null)
+        {
+            UFE.SetPlayer1(playerCharaInfo1);
+        }
+        if (playerCharaInfo2 != null)
+        {
+            UFE.SetPlayer2(playerCharaInfo2);
+        }
+        
+
+        // PVPを開始
+        UFE.StartPlayerVersusPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
