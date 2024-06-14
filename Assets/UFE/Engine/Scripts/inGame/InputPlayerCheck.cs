98f8e34fbcddaa5747a57d9ab56e877069fcc601
TashiroRenya <22cu0125@jec.ac.jp>
2024-06-14 19:18:33 +0900

    マップ処理を追加したり、キャラのセットアップの続きをしたりインプットを直そうとして失敗したりしました

---
+++ Assets/UFE/Engine/Scripts/inGame/InputPlayerCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UFE3D;
+using UnityEngine;
+
+public class InputPlayerCheck : MonoBehaviour
+{
+    //変数宣言
+
+    private void Start()
+    {
+
+    }
+}
---
+++ Assets/UFE/Engine/Scripts/inGame/StartInGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UFE3D;
+
+public class StartInGame : MonoBehaviour
+{
+    //変数宣言
+    [SerializeField]
+    private UFE3D.CharacterInfo p1CharAsset;
+    [SerializeField]
+    private UFE3D.CharacterInfo p2CharAsset;
+    [SerializeField]
+    private StageOptions stagePrefab;
+    [SerializeField]
+    private float gameFadeDuration;
+
+    private void Start()
+    {
+        UFE.gameMode = GameMode.VersusMode; // or GameMode.TrainingMode if you want Training.
+        UFE.config.player1Character = p1CharAsset; // or whatever variable you have the Character Asset file set 
+        UFE.config.player2Character = p2CharAsset; // or whatever variable you have the Character Asset file set 
+        UFE.config.selectedStage = stagePrefab; // or whatever variable you have the Stage Prefab file set
+        UFE.SetCPU(1, false); // Set to true for AI on P1
+        UFE.SetCPU(2, false); // Set to true for AI on P2
+        UFE.StartGame(gameFadeDuration);
+    }
+}
