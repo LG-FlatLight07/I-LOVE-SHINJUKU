541b1d20c44df84fc8ebe1e386cff4188cc04b63
GSDCow <makuradenetaina@gmail.com>
2024-06-21 23:54:34 +0900

    ビルド用PUSH

---
+++ Assets/_Matching/FPSLock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLock : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Vsync Count を 0にすることにより、FPS を固定できるようになる
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+}
