3966d7ac1962153d7eaade979d3b24ce85210fa3
TashiroRenya <22cu0125@jec.ac.jp>
2024-06-21 13:42:29 +0900

    ﻿カメラの変更と、スカイボックスのマテリアルを変更できるようにしてるので、そのスクリプトをマップに適応させておいて

---
+++ Assets/UFE/Engine/Scripts/inGame/CameraSetting.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetting : MonoBehaviour
+{
+    [SerializeField]
+    private float stageCammeraFarClipPlane = 100;
+    //カメラの
+    //位置
+    [SerializeField]
+    private Vector3 stageCameraTransform = new Vector3(0, 9, -53);
+    //カメラの回転
+    [SerializeField]
+    private Vector3 stageCameraRotation = new Vector3(7, 0, 0);
+    //見ているかどうか
+    private bool stageCamEnableLookAt = true;
+    //ズームが有効かどうか
+    private bool stageCamEnableZoom = true;
+    //ズーム距離
+    private float stageCamFieldOfView = 12;
+    //最小ズーム距離
+    [SerializeField]
+    private float stageCamMinimumZoom = 40;
+    //最大ズーム距離
+    [SerializeField]
+    private float stageCamMaximumZoom = 60;
+    //プレイヤーとカメラの距離
+    [SerializeField]
+    private float stageCamMaximumPlayerDistance = 18;
+
+
+    private void Awake()
+    {
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().farClipPlane = stageCammeraFarClipPlane;
+
+        //UFEのカメラにそれぞれの設定を適応
+        UFE.config.cameraOptions.initialDistance = stageCameraTransform;
+        UFE.config.cameraOptions.initialRotation = stageCameraRotation;
+        UFE.config.cameraOptions.enableLookAt = stageCamEnableLookAt;
+        UFE.config.cameraOptions.initialFieldOfView = stageCamFieldOfView;
+        UFE.config.cameraOptions.minZoom = stageCamMinimumZoom;
+        UFE.config.cameraOptions.maxZoom = stageCamMaximumZoom;
+        UFE.config.cameraOptions.enableZoom = stageCamEnableZoom;
+        UFE.config.cameraOptions._maxDistance = stageCamMaximumPlayerDistance;
+    }
+}
---
+++ Assets/UFE/Engine/Scripts/inGame/SkyBoxSetting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxSetting : MonoBehaviour
+{
+    [SerializeField] 
+    private Material skyBoxMaterial;
+
+    private void Awake()
+    {
+        //スカイボックスのマテリアル変更
+        RenderSettings.skybox = skyBoxMaterial;
+    }
+}
