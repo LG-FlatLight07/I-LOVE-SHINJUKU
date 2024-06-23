97a8ab7964b73222c93d639fecf5c1dc77d18ad0
GSDCow <makuradenetaina@gmail.com>
2024-06-24 03:31:36 +0900

    うおおおおおおおお

--- Assets/_Map/GeneralMapLoader.cs
+++ Assets/_Map/GeneralMapLoader.cs
@@ -17,40 +17,25 @@ public class GeneralMapLoader : MonoBehaviour
         
         if (Input.GetKeyDown("2"))
         {
-            SceneManager.LoadScene("2_VSMobIngame"); 
-        }
-        
-        if (Input.GetKeyDown("3"))
-        {
             SceneManager.LoadScene("3_VSMobResult");
         }
 
-        if (Input.GetKeyDown("4"))
+        if (Input.GetKeyDown("3"))
         {
             SceneManager.LoadScene("4_VSKagami1Matching");
         }
 
-        if (Input.GetKeyDown("5"))
-        {
-            SceneManager.LoadScene("5_VSKagami1Ingame");
-        }
-
-        if (Input.GetKeyDown("6"))
+        if (Input.GetKeyDown("4"))
         {
             SceneManager.LoadScene("6_VSKagami1Result");
         }
 
-        if (Input.GetKeyDown("7"))
+        if (Input.GetKeyDown("5"))
         {
             SceneManager.LoadScene("7_VSKagami2Matching");
         }
 
-        if (Input.GetKeyDown("8"))
-        {
-            SceneManager.LoadScene("8_VSKagami2Ingame");
-        }
-
-        if (Input.GetKeyDown("9"))
+        if (Input.GetKeyDown("6"))
         {
             SceneManager.LoadScene("9_VSKagami2Result");
         }
