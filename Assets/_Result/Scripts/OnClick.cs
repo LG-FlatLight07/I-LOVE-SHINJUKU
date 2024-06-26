30ead57205fd21c99b948ff9e1b4706aaba58152
DESKTOP-4RVAV3U\玉置　弥雅 <22cu0223@jec.ac.jp>
2024-06-26 15:29:46 +0900

    リザルトフェードインと母田qン追加

---
+++ Assets/_Result/Scripts/OnClick.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnClick : MonoBehaviour
+{
+    // インスペクタビューから設定するシーン名
+    public string sceneName;
+
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
---
+++ Assets/_Result/Scripts/ResultFadain.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+//マッチングシステム 
+public class ResultFadain : MonoBehaviour
+{
+    float Speed = 0.01f;
+    float red, green, blue, alfa;
+
+    public bool Out = false;
+    public bool In = false;
+
+    public SpriteRenderer fadeImage;
+
+    [SerializeField] //開始時ディレイ
+    private float StartDelayTime = 3;
+    [SerializeField] //終了時ディレイ
+    private float EndDelayTime = 6;
+
+    void Start()
+    {
+        //スプライトレンダラー
+        fadeImage = GetComponent<SpriteRenderer>();
+        red = fadeImage.color.r;
+        green = fadeImage.color.g;
+        blue = fadeImage.color.b;
+        alfa = fadeImage.color.a;
+
+        StartCoroutine(StartDelay());
+
+    }
+
+    void Update()
+    {
+
+        if (In)
+        {
+            FadeIn();
+        }
+
+       /* if (Out)
+        {
+            FadeOut();
+        }*/
+    }
+
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(StartDelayTime);
+        In = true;
+        StartCoroutine(EndDelay());
+    }
+
+    IEnumerator EndDelay()
+    {
+        yield return new WaitForSeconds(EndDelayTime);
+        Out = true;
+
+        //マップを開く
+        Invoke("ChangeScene", 3f);
+    }
+
+    void FadeIn()
+    {
+        alfa -= Speed;
+        Alpha();
+        if (alfa <= 0)
+        {
+            In = false;
+        }
+    }
+
+  /*  void FadeOut()
+    {
+        alfa += Speed;
+        Alpha();
+        if (alfa >= 1)
+        {
+            Out = false;
+        }
+    }*/
+
+    void Alpha()
+    {
+        fadeImage.color = new Color(red, green, blue, alfa);
+    }
+}
